c55044e5d3ca63212703f5eebbba4ee3db0ecdd8
pavan <mpavan2@gmail.com>
2022-12-01 01:39:29 -0500

    Added Doxygen Comments

--- include/beginner_tutorials/publisher_member_function.hpp
+++ include/beginner_tutorials/publisher_member_function.hpp
@@ -1,7 +1,24 @@
-/* Copyright 2022
- * Author
- * Pavan Mantripragada
- */
+// MIT License
+
+// Copyright (c) 2022 Pavan Mantripragada
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #pragma once
 #include <chrono>
@@ -50,6 +67,10 @@ class MinimalPublisher : public rclcpp::Node {
       const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request>
           request,
       std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response);
+  /**
+   * @brief method for
+   * publishing static transform
+   */
   void make_transforms();
   /** @brief timer pointer */
   rclcpp::TimerBase::SharedPtr timer_;
@@ -57,6 +78,7 @@ class MinimalPublisher : public rclcpp::Node {
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   /** @brief server pointer */
   rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
+  /** @brief tf broadcaster pointer */
   std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
   /** @brief count of no. of msgs published */
   size_t count_;
